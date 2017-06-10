366394c95491067ead82617864aedb38e3266f9f
Ray Climie <seederwood@gmail.com>
2017-06-10 16:03:42 -0300

    customise admin dashboard forms

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class AdminUserDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -11,7 +11,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
     posts: Field::HasMany,
     id: Field::Number,
     email: Field::String,
-    encrypted_password: Field::String,
+    password: Field::String,
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
     remember_created_at: Field::DateTime,
@@ -24,61 +24,45 @@ class AdminUserDashboard < Administrate::BaseDashboard
     last_name: Field::String,
     type: Field::String,
     created_at: Field::DateTime,
-    updated_at: Field::DateTime,
+    updated_at: Field::DateTime
   }.freeze
 
   # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = [
-    :posts,
-    :id,
-    :email,
-    :encrypted_password,
+
+  COLLECTION_ATTRIBUTES = %i[
+    posts
+    id
+    last_name
+    first_name
+    email
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [
-    :posts,
-    :id,
-    :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :first_name,
-    :last_name,
-    :type,
-    :created_at,
-    :updated_at,
+
+  SHOW_PAGE_ATTRIBUTES = %i[
+    posts
+    id
+    email
+    sign_in_count
+    current_sign_in_at
+    last_sign_in_at
+    current_sign_in_ip
+    last_sign_in_ip
+    first_name
+    last_name
+    created_at
+    updated_at
+    type
   ].freeze
 
   # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = [
-    :posts,
-    :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :first_name,
-    :last_name,
-    :type,
+
+  FORM_ATTRIBUTES = %i[
+    email
+    password
+    first_name
+    last_name
+    type
   ].freeze
 
   # Overwrite this method to customize how admin users are displayed
