72978b44d4efc0384e46579e38003c3dcadd64cf
Ray Climie <seederwood@gmail.com>
2017-07-11 15:55:01 -0300

    setup basic layout of employee homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  def index
-    search_term = params[:search].to_s.strip
-    resources = Administrate::Search.new(resource_resolver, search_term).run
-    resources = order.apply(resources)
-    resources = resources.page(params[:page]).per(records_per_page)
-    page = Administrate::Page::Collection.new(dashboard, order: order)
-
-    render locals: {
-      resources: resources,
-      search_term: search_term,
-      page: page
-    }
+  def admin_types
+    ['AdminUser']
   end
 
   private
