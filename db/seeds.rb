6fe519d5268c9f53223b370252d406a81d5c1cf0
Ray Climie <seederwood@gmail.com>
2017-07-13 15:03:33 -0300

    implement rake schedule for sms notifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-@user = User.create(email: 'test@test.com',
+@employee = Employee.create(email: 'test@test.com',
                     password: 'asdfasdf',
                     password_confirmation: 'asdfasdf',
                     first_name: 'Jon',
                     last_name: 'Snow',
                     phone: '9737073878')
 
-puts 'user created'
+puts 'employee created'
 
 AdminUser.create(email: 'diva@barty.lina',
                  password: 'asdfasdf',
@@ -14,18 +14,18 @@ AdminUser.create(email: 'diva@barty.lina',
                  last_name: 'Beaner',
                  phone: '9737073878')
 
-puts 'Admin User Created'
+puts 'Admin user Created'
 
 
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts '3 Audit Logs have been created'
 
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} Iceland plaid letterpress mlkshk flannel thundercats. Microdosing salvia keytar hella plaid. Heirloom try-hard literally, small batch plaid slow-carb butcher austin flexitarian messenger bag. Offal tattooed next level, prism crucifix vice direct trade twee selvage cliche.", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} Iceland plaid letterpress mlkshk flannel thundercats. Microdosing salvia keytar hella plaid. Heirloom try-hard literally, small batch plaid slow-carb butcher austin flexitarian messenger bag. Offal tattooed next level, prism crucifix vice direct trade twee selvage cliche.", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts '100 Posts have been created'
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -2,23 +2,18 @@ namespace :notification do
   desc 'sends SMS notification to employees asking them to log overtime'
   task sms: :environment do
     if Time.now.sunday?
+      employees = Employee.all
+      notification_message = "Please log in to the overtime dashboard to request overtime or confirm your hours for last week: https://rc-ot-app.herokuapp.com"
+    
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+        puts notification_message
+      end
     end
-
-    # 1. schedule to run on Sunday at 5pm
-    # 2. Iterate all employees
-    # 3. skip AdminUsers
-    # 4. send a message that has instructions and a link to log time
-    # User.all.each do |user|
-    #   SmsTool.send_sms()
-    # end
   end
 
   desc 'sends mail notification to managers (admin users) daily to inform of pending overtime requests'
   task manager_email: :environment do
-    # 1. iterate over the list of pending overtime requests
-    # 2. Check to see if there are any requests
-    # 3. iterate over list of admin users/managers
-    # 4. send email
     submitted_posts = Post.submitted
     admin_users = AdminUser.all
 
